ca88eb4f5a60f21a011fdf9162d6e4869d9d2b13
Erick-Franco <rrickquispe@gmail.com>
2025-04-23 16:50:10 -0500

    Exmaen14.9

--- eqm-estudiante-service/src/main/java/com/example/eqmestudianteservice/controller/EstudianteController.java
+++ eqm-estudiante-service/src/main/java/com/example/eqmestudianteservice/controller/EstudianteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/estudiantes")
+@RequestMapping("/estudiantes")
 public class EstudianteController {
 
     @Autowired
